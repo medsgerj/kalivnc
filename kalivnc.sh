2c103971f3668a6b85fd054802269066f14cb7d5
medsgerj <medsgerj@gmail.com>
2017-03-20 07:52:06 -0700

    minor changes

--- kalivnc.sh
+++ kalivnc.sh
@@ -12,13 +12,21 @@ useradd vncserver -m
 echo "[*] Set a password for vncserver user:"
 passwd vncserver
 
-echo "[*] Set a password for vncserver access:"
-/usr/bin/vncpasswd vncserver
+echo "[*] Set up vncserver for first time."
+su - vncserver -c "/usr/bin/vncserver :1"
 
-echo "[*] Creating .Xresources file."
-xresources=/home/vncserver/.Xresources
-touch $xresources
-chown vncserver:vncserver $xresources
+#echo "[*] Creating /home/vncserver/.vnc/."
+#vncdir=/home/vncserver/.vnc
+#mkdir $vncdir
+#chown -R vncserver:vncserver $vncdir
+
+#echo "[*] Copying passwd file."
+#cp /root/
+
+#echo "[*] Creating .Xresources file."
+#xresources=/home/vncserver/.Xresources
+#touch $xresources
+#chown vncserver:vncserver $xresources
 
 echo "[*] Creating .xstartup file."
 xstartup=/home/vncserver/.vnc/xstartup
@@ -45,7 +53,7 @@ echo "Description=vncserver" >> $vcs
 echo "After=network.target" >> $vcs
 echo "" >> $vcs
 echo "[Service]" >> $vcs
-echo "Type=oneshot" >> $vcs
+echo "Type=forking" >> $vcs
 echo "ExecStart=/usr/bin/vncserver :1 -localhost" >> $vcs
 echo "ExecStop=/usr/bin/vncserver -kill :1" >> $vcs
 echo "User=vncserver" >> $vcs
